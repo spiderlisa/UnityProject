44e236c35076cfc5a64b475985fb77c26b00126c
spiderlisa <lisuha.zlk3008@gmail.com>
2018-05-25 19:32:17 +0300

    HomeWork 1

---
+++ Assets/Content/Rabbit/HeroFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroFollow : MonoBehaviour {
+
+	public HeroRabbit rabit;
+
+	void Update () {
+		Transform rabit_transform = rabit.transform;
+
+		Transform camera_transform = this.transform;
+
+		Vector3 rabit_position = rabit_transform.position;
+		Vector3 camera_position = camera_transform.position;
+
+		camera_position.x = rabit_position.x;
+		camera_position.y = rabit_position.y;
+
+		camera_transform.position = camera_position;
+	}
+}
